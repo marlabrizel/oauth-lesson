9a6dc467f41e9c464d702f7335bd54293ce9db20
Marla Brizel <marlabrizel@gmail.com>
2015-09-08 10:07:43 -0600

    Add find or create from auth method
    Look for or create user based on provider and uid
    Everytime the user logs in, update the email, nickname, image url, and
    token
    
    Return user after saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  def self.find_or_create_from_auth(data)
+    user = User.find_or_create_by(provider: data.provider, uid: data.uid)
+    # if there is a user in the db that matches the provider and uid, return that user. otherwise, create the record
+    # check for both provider and uid because you can have the same uid for different providers
+
+    user.email     = data.info.email
+    user.nickname  = data.info.nickname
+    user.image_url = data.info.image
+    user.token     = data.credentials.token
+    user.save!
+
+    user
+  end
 end
