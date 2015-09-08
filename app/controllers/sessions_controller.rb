a07f827ab921e559b8ad5b6786bfc39726e18b4a
Marla Brizel <marlabrizel@gmail.com>
2015-09-08 10:25:39 -0600

    add logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,17 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth(oauth_data)
-
     if user
       session[:user_id] = user.id
       redirect_to dashboard_path
     else
       redirect_to root_path
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 
   private
